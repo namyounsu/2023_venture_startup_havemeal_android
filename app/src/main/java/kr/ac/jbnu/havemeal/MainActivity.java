10a28158d609fa8346bf0ea1123d277a8cfd2321
NamYounSu <nys6635@gmail.com>
2023-06-28 01:25:51 +0900

    initial commit
    Signed-off-by: NamYounSu <nys6635@gmail.com>

---
+++ app/src/main/java/kr/ac/jbnu/havemeal/MainActivity.java
@@ -0,0 +1,20 @@
+package kr.ac.jbnu.havemeal;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
